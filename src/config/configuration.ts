948cb35d7532eebf54e95112d7095b8a0fe0a7cd
Mykyta Vostrikov <mykyta.vostrikov@sigma.software>
2024-04-19 20:31:20 +0300

    configurated k8s for app

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -3,9 +3,9 @@ const configuration = () => ({
     port: parseInt(process.env.APP_PORT, 10) || 3000,
   },
   database: {
-    name: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    name: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
     host: process.env.DB_HOST,
     port: parseInt(process.env.DB_PORT, 10) || 5432,
   },
